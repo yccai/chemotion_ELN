3a244c0aa806ea432d245f6d080320e76b8ea5ac
jasonych99 <jasonych99@gmail.com>
2016-06-06 17:28:44 +0200

    build report

--- app/assets/javascripts/components/models/Report.js
+++ app/assets/javascripts/components/models/Report.js
@@ -2,10 +2,13 @@ import Element from './Element';
 
 export default class Report extends Element {
   static buildEmpty() {
-    return new Report({
+    let report = new Report({
       type: 'report',
-      name: 'New Report',
-      reactions: []
-    })
+      name: 'New Report' + new Date(),
+      description: '',
+      elements: []
+    });
+
+    return report;
   }
 }
