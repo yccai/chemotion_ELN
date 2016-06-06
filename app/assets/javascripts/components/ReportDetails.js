9ec3852c1cd6110ab5a17e8b527ea353f57b429d
jasonych99 <jasonych99@gmail.com>
2016-06-06 15:36:59 +0200

    refine ReportDetails

--- app/assets/javascripts/components/ReportDetails.js
+++ app/assets/javascripts/components/ReportDetails.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import {Col, Row, Panel, ListGroup, ListGroupItem, ButtonToolbar, Button, Tabs, Tab} from 'react-bootstrap';
+import StickyDiv from 'react-stickydiv'
+
 import ElementCollectionLabels from './ElementCollectionLabels';
 import ElementAnalysesLabels from './ElementAnalysesLabels';
 import ElementActions from './actions/ElementActions';
@@ -12,56 +14,33 @@ import UIStore from './stores/UIStore';
 import UIActions from './actions/UIActions';
 import SVG from 'react-inlinesvg';
 import Utils from './utils/Functions';
-
-
 import XTab from "./extra/ReactionDetailsXTab";
 import XTabName from "./extra/ReactionDetailsXTabName";
 
-import StickyDiv from 'react-stickydiv'
 
-export default class ReactionDetails extends Component {
+export default class ReportDetails extends Component {
   constructor(props) {
     super(props);
-    const {reaction} = props;
-    this.state = {
-      reaction,
-      reactionPanelFixed: false
-    };
+    this.state = {};
   }
 
   componentDidMount() {
-    const {reaction} = this.state;
-  }
 
-  componentWillReceiveProps(nextProps) {
-    const {reaction} = this.state;
-    const nextReaction = nextProps.reaction;
-    if (nextReaction.id != reaction.id || nextReaction.updated_at != reaction.updated_at) {
-      this.setState({
-        reaction: nextReaction
-      });
-    }
   }
 
   render() {
-    const {reaction} = this.state;
-    const svgContainerStyle = {
-      textAlign: 'center'
-    };
-    const submitLabel = (true) ? "Create" : "Save";
+    const submitLabel = (true) ? "Create" : "Save"; // TBD
     const style = {height: '220px'};
-    let extraTabs =[];
-    for (let j=0;j < XTab.TabCount;j++){
-      extraTabs.push((i)=>this.extraTab(i))
-    }
+
     return (
       <StickyDiv zIndex={2}>
         <Panel className="panel-fixed"
-               header="Report QQ Details"
+               header="Report"
                bsStyle={true ? 'info' : 'primary'}>
           <Button bsStyle="danger"
                   bsSize="xsmall"
-                  className="button-right" >
+                  className="button-right" 
+                  onClick={this.closeDetails.bind(this)}>
             <i className="fa fa-times"></i>
           </Button>
           <Row>
@@ -69,10 +48,14 @@ export default class ReactionDetails extends Component {
               <h3>HAHAH.....</h3>
 
             </Col>
-            {}
           </Row>
         </Panel>
       </StickyDiv>
     );
+  }
+
+  closeDetails() {
+    UIActions.deselectAllElements();
+    Aviator.navigate(`/collection/all`);
   }
 }
